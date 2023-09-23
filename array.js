7b96bcba45a7dc4d5d6d020791981836138f76ac
Quan Do <quandovan01@gmail.com>
2023-09-23 22:33:34 +0700

    array

---
+++ array.js
@@ -0,0 +1,5 @@
+var mobile = ["HTC", "Nokia"];
+var motorbike = ["Honda", "Yamaha", "Suzuki"];
+var SinhVien = ["Nguyễn Thành Nhân", 1993];
+var city = ["Cần Thơ", "Vĩnh Long"];
+var data = motorbike.concat(city, SinhVien, mobile);
