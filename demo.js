35fa483e84cfa0a5a3f53ed00d721f531f043f6c
Quan Do <quandovan01@gmail.com>
2023-09-25 10:21:51 +0700

    update uncheck-all

--- demo.js
+++ demo.js
@@ -8,17 +8,6 @@ for (var y = 0; y < as2D.length; y++) {
 }
 sOut += "</table>";
 
-// Chức năng chọn hết
-document.getElementById("btn1").onclick = function () {
-   // Lấy danh sách checkbox
-   var checkboxes = document.getElementsByName('name[]');
-
-   // Lặp và thiết lập checked
-   for (var i = 0; i < checkboxes.length; i++) {
-      checkboxes[i].checked = true;
-   }
-};
-
 // Chức năng bỏ chọn hết
 document.getElementById("btn2").onclick = function () {
    // Lấy danh sách checkbox
