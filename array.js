8b35ff3e465810d4721849f463f19495c8b59b11
Quan Do <quandovan01@gmail.com>
2023-09-25 09:44:17 +0700

    assign value to input

--- array.js
+++ array.js
@@ -3,3 +3,9 @@ var motorbike = ["Honda", "Yamaha", "Suzuki"];
 var SinhVien = ["Nguyễn Thành Nhân", 1993];
 var city = ["Cần Thơ", "Vĩnh Long"];
 var data = motorbike.concat(city, SinhVien, mobile);
+
+// Gán giá trị cho thẻ input
+function setDomain() {
+    let domain = 'freetuts.net';
+    document.getElementById('domain').setAttribute('value', domain);
+}
