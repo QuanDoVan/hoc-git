d016c20d3d37bbd4c309cdee730342439b8430ef
Quan Do <quandovan01@gmail.com>
2023-09-25 10:55:44 +0700

    loop-while

--- loop.js
+++ loop.js
@@ -12,3 +12,22 @@ function print_number() {
     }
     document.getElementById("result").innerHTML = html;
 }
+
+function tinh_tong(n)
+{
+    var tong = 0;
+    var index = 0;
+ 
+    while (index <= n)
+    {
+        // Nếu số chẵn thì cộng vào
+        if (index % 2 == 0){
+            tong += index;
+        }
+ 
+        // tăng biến đếm
+        index++;
+    }
+ 
+    document.write("Tổng số chẵn từ 1 tới " + n + " là: " + tong + "<br/>");
+}
