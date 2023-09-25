c9ecdc4b04f924de7b108b5a0b860c69bb4538e8
Quan Do <quandovan01@gmail.com>
2023-09-25 10:18:06 +0700

    check-all

--- demo.js
+++ demo.js
@@ -1,9 +1,20 @@
-var sOut="<table border=2>";
-   for (var y=0; y<as2D.length; y++ ) {
-      sOut += "<tr>";
-      for (var x=0; x<as2D[y].length; x++ ) {
-         sOut += "<td>" + as2D[y][x] + "</td>";
-      }
-       sOut += "</tr>";
+var sOut = "<table border=2>";
+for (var y = 0; y < as2D.length; y++) {
+   sOut += "<tr>";
+   for (var x = 0; x < as2D[y].length; x++) {
+      sOut += "<td>" + as2D[y][x] + "</td>";
    }
-   sOut += "</table>";
+   sOut += "</tr>";
+}
+sOut += "</table>";
+
+// Chức năng chọn hết
+document.getElementById("btn1").onclick = function () {
+   // Lấy danh sách checkbox
+   var checkboxes = document.getElementsByName('name[]');
+
+   // Lặp và thiết lập checked
+   for (var i = 0; i < checkboxes.length; i++) {
+      checkboxes[i].checked = true;
+   }
+};
